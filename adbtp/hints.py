ed9034290513f485ded8209e7d2fdfeff1716eb2
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-08-27 18:17:52 -0700

    Add hints module for package wide type hinting.

---
+++ adbtp/hints.py
@@ -0,0 +1,29 @@
+"""
+    adbtp.hints
+    ~~~~~~~~~~~
+
+    Contains type hint definitions used across modules in this package.
+"""
+import typing
+
+# pylint: disable=invalid-name,no-member,unsubscriptable-object
+
+#: Type hint that is an alias for the built-in :class:`~bool` type.
+Bool = bool
+
+
+#: Type hint that is an alias for the built-in :class:`~float` type.
+Float = float
+
+
+#: Type hint that is an alias for the built-in :class:`~int` type.
+Int = int
+
+
+#: Type hint that is an alias for the built-in :class:`~str` type.
+Str = str
+
+
+#: Type hint that defines an optional integer value that represents
+#: a timeout value to a transport.
+Timeout = typing.Optional[int]
