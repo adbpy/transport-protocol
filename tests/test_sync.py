515f89a9c44401d7432a07ba6bd21f3ffc47d5eb
Andrew Hawker <andrew.r.hawker@gmail.com>
2018-01-31 21:44:04 -0800

    Add stub test module for the sync protocol module.

---
+++ tests/test_sync.py
@@ -0,0 +1,14 @@
+"""
+    test_sync
+    ~~~~~~~~~
+
+    Tests for the :mod:`~adbtp.sync` module.
+"""
+import pytest
+
+from adbtp import sync
+
+
+@pytest.mark.xfail(reason='Not implemented')
+def test_stub():
+    assert False
